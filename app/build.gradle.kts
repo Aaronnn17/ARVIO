78bbea5af0574fef86914709ba5f624c6cf129b8
Arvin <arvin@arflix.local>
2026-07-30 14:57:28 +0200

    chore(release): prepare v1.9.983

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         // Fire TV devices can be as low as Android 7.1 (API 25) or lower depending on model/OS.
         minSdk = 23
         targetSdk = 36
-        versionCode = 305
-        versionName = "1.9.982"
+        versionCode = 306
+        versionName = "1.9.983"
         buildConfigField("String", "GITHUB_OWNER", "\"ProdigyV21\"")
         buildConfigField("String", "GITHUB_REPO", "\"ARVIO\"")
         buildConfigField("Boolean", "FEATURE_PLUGINS_ENABLED", "false")
